2abfe640f67f3083bbf23f17beaf9e8e291f5212
Aaqil Khan <en21cs3l1003@medicaps.ac.in>
2024-04-29 19:42:01 +0530

    logout refactored && resume completed

--- src/path.js
+++ src/path.js
@@ -1,5 +1,5 @@
 //const path = "http://192.168.29.27:4000";
-//onst path = "https://masterly-api.vercel.app";
-const path = "http://192.168.1.10:4000";
+onst path = "https://masterly-api.vercel.app";
+//const path = "http://192.168.1.10:4000";
 //const path = "http://localhost:4000";
 export default path;
